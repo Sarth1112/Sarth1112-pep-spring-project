2f8a499e29effa427cf7b642c9b078c14e664be4
Sarthak Gohel <sarthakgohel5555@gmail.com>
2025-06-19 14:38:23 +0000

    logRegDone

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,16 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.exception.AuthenticationFailedException;
+import com.example.exception.DuplicateUsernameException;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +18,39 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+@RestController
 public class SocialMediaController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerAccount(@RequestBody Account account){
+        try {
+            Account createdAccount = accountService.addUser(account);
+            return ResponseEntity.ok(createdAccount);
+            
+        } catch (DuplicateUsernameException e) {
+            return ResponseEntity.status(409).body("Username already exists.");
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Registration Failed.");
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginAccount(@RequestBody Account account){
+
+        try{
+            Account loggedInAccount = accountService.login(account.getUsername(), account.getPassword());
+            return ResponseEntity.ok(loggedInAccount);
+        }catch(AuthenticationFailedException e){
+            return ResponseEntity.status(401).body("Unauthorized");
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,64 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+import com.example.entity.Account;
+import com.example.exception.AuthenticationFailedException;
+import com.example.exception.DuplicateUsernameException;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    /*
+     - The registration will be successful if and only if the username is not blank, the password is at least 4 characters long, and an Account with that username does not already exist. If all these conditions are met, the response body should contain a JSON of the Account, including its accountId. The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+     - If the registration is not successful due to a duplicate username, the response status should be 409. (Conflict)
+     - If the registration is not successful for some other reason, the response status should be 400. (Client error)
+     */
+
+    public Account addUser(Account account){
+
+        if (account.getUsername() == null || account.getUsername().isBlank() || account.getPassword() == null || account.getPassword().length() < 4) {
+            throw new IllegalArgumentException("Invalid input"); // Will translate to 400 in controller
+        }
+
+        if (accountRepository.findByUsername(account.getUsername()).isPresent()) {
+            throw new DuplicateUsernameException("Username already exists"); // Custom exception
+        }
+
+        return accountRepository.save(account);
+
+    }
+
+    /*
+     ## 2: Our API should be able to process User logins.
+
+As a user, I should be able to verify my login on the endpoint POST localhost:8080/login. The request body will contain a JSON representation of an Account.
+
+- The login will be successful if and only if the username and password provided in the request body JSON match a real account existing on the database. If successful, the response body should contain a JSON of the account in the response body, including its accountId. The response status should be 200 OK, which is the default.
+- If the login is not successful, the response status should be 401. (Unauthorized)
+     */
+
+     public Account login(String username, String password){
+        Optional<Account> existing = accountRepository.findByUsername(username);
+
+        if(existing.isPresent() && existing.get().getPassword().equals(password)){
+           return existing.get();
+        }else {
+            throw new AuthenticationFailedException("Invalid input!");
+        }
+
+    }
+
+
+
 }
